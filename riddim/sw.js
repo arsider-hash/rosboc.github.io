13374e50813d73a9cfb28154aab0df02d0c31d1c
arsider-hash <arsider.cam@gmail.com>
2026-09-18 17:52:56 +0200

    Bump cache v4.2.1 after XY fix

--- riddim/sw.js
+++ riddim/sw.js
@@ -1,4 +1,4 @@
-const C='arsider-riddim-v42';
+const C='arsider-riddim-v421';
 const A=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C&&k.startsWith('arsider-riddim')).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
